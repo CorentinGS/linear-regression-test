feb2a8d83306b2939e4c7866358fc0498fa77131
Yume <yume.network@protonmail.com>
2022-03-20 18:54:41 +0100

    update

--- main.py
+++ main.py
@@ -1,46 +1,33 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.tree import DecisionTreeRegressor
+import pandas as pd
+from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
-from sklearn import datasets, linear_model, metrics
+from sklearn.model_selection import train_test_split
 
 
 def main():
     print("hello world")
 
-    dataset = pd.read_csv("dt3.csv")
+    dataset = pd.read_csv("data2.csv")
     dataset.head()
 
-    # sns.histplot(dataset["Quality"])
-
-    # plt.show()
-
-    #x_ = dataset[['CID', 'UID']]
-    x_ = dataset[['efactor',  'repetition', 'UID', 'CID']]
+    x_ = dataset[["user_id", "card_id", "quality", "repetition", "efactor"]]
     y = dataset['interval']
 
-    # x_ = PolynomialFeatures(degree=3, include_bias=False).fit_transform(x)
+    x_train, x_test, y_train, y_test = train_test_split(x_, y, test_size=0.4, random_state=30)
 
-    x_train, x_test, y_train, y_test = train_test_split(x_, y, test_size = 0.3, random_state = 30)
+    # dtree = DecisionTreeRegressor(max_depth=10, min_samples_leaf=0.13, random_state=3)
+    # dtree.fit(x_train, y_train)
 
-    #dtree = DecisionTreeRegressor(max_depth=10, min_samples_leaf=0.13, random_state=3)
-    #dtree.fit(x_train, y_train)
+    model_rf = RandomForestRegressor(n_estimators=150, max_features=1, oob_score=True, random_state=30)
+    model_rf.fit(x_train, y_train)
 
-    model_rf = RandomForestRegressor(n_estimators=150,  max_features=1, oob_score=True, random_state=30)
-    model_rf.fit(x_train, y_train) 
-
-
-    y_pred_mlr= model_rf.predict(x_test)
-
+    y_pred_mlr = model_rf.predict(x_test)
 
     mlr_diff = pd.DataFrame({'Actual value': y_test, 'Predicted value': y_pred_mlr})
     print(mlr_diff)
 
-    print('R squared value of the model: {:.2f}'.format(model_rf.score(x_,y)*100))
+    print('R squared value of the model: {:.2f}'.format(model_rf.score(x_, y) * 100))
 
     meanAbErr = metrics.mean_absolute_error(y_test, y_pred_mlr)
     meanSqErr = metrics.mean_squared_error(y_test, y_pred_mlr)
@@ -50,7 +37,8 @@ def main():
     print('Mean Square Error:', meanSqErr)
     print('Root Mean Square Error:', rootMeanSqErr)
 
-    toto = model_rf.predict(([[2.58463262, 6, 6, 15]]))
+    toto = model_rf.predict(([[6, 280, 5, 2, 2.9727996826171875]]))
     print(toto)
+
 
 main()
