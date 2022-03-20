466d6c04300bde2a758b9c720635ffa578a26b44
Yume <yume.network@protonmail.com>
2022-03-20 19:17:44 +0100

    update

--- main.py
+++ main.py
@@ -3,42 +3,54 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+# Compare Algorithms
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+def LDA():
+    model = LinearDiscriminantAnalysis()
+
 
 
 def main():
-    print("hello world")
-
+    # load dataset
+    names = ["user_id", "card_id", "quality", "repetition", "efactor", "interval"]
     dataset = pd.read_csv("data2.csv")
     dataset.head()
 
-    x_ = dataset[["user_id", "card_id", "quality", "repetition", "efactor"]]
-    y = dataset['interval']
-
-    x_train, x_test, y_train, y_test = train_test_split(x_, y, test_size=0.4, random_state=30)
-
-    # dtree = DecisionTreeRegressor(max_depth=10, min_samples_leaf=0.13, random_state=3)
-    # dtree.fit(x_train, y_train)
-
-    model_rf = RandomForestRegressor(n_estimators=150, max_features=1, oob_score=True, random_state=30)
-    model_rf.fit(x_train, y_train)
-
-    y_pred_mlr = model_rf.predict(x_test)
-
-    mlr_diff = pd.DataFrame({'Actual value': y_test, 'Predicted value': y_pred_mlr})
-    print(mlr_diff)
-
-    print('R squared value of the model: {:.2f}'.format(model_rf.score(x_, y) * 100))
-
-    meanAbErr = metrics.mean_absolute_error(y_test, y_pred_mlr)
-    meanSqErr = metrics.mean_squared_error(y_test, y_pred_mlr)
-    rootMeanSqErr = np.sqrt(metrics.mean_squared_error(y_test, y_pred_mlr))
-
-    print('Mean Absolute Error:', meanAbErr)
-    print('Mean Square Error:', meanSqErr)
-    print('Root Mean Square Error:', rootMeanSqErr)
-
-    toto = model_rf.predict(([[6, 280, 5, 2, 2.9727996826171875]]))
-    print(toto)
+    array = dataset.values
+    X = array[:, 0:5]
+    Y = array[:, 5]
+    # prepare configuration for cross validation test harness
+    seed = 7
+    # prepare models
+    models = [('LR', LogisticRegression()), ('LDA', LinearDiscriminantAnalysis()), ('KNN', KNeighborsClassifier()),
+              ('CART', DecisionTreeClassifier()), ('NB', GaussianNB()), ('SVM', SVC())]
+    # evaluate each model in turn
+    results = []
+    names = []
+    scoring = 'accuracy'
+    for name, model in models:
+        kfold = model_selection.KFold(n_splits=10, random_state=None)
+        cv_results = model_selection.cross_val_score(model, X, Y, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+    # boxplot algorithm comparison
+    fig = plt.figure()
+    fig.suptitle('Algorithm Comparison')
+    ax = fig.add_subplot(111)
+    plt.boxplot(results)
+    ax.set_xticklabels(names)
+    plt.show()
 
 
 main()
